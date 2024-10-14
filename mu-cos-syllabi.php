52630be61eaf27c28d3212335bfd2f2dbd3b6499
Christopher McComas <mccomas.chris@gmail.com>
2024-10-14 15:17:40 -0400

    wip

--- mu-cos-syllabi.php
+++ mu-cos-syllabi.php
@@ -6,15 +6,15 @@
  * Author: Nick Slate
  * Version: 1.2
  *
- * @package MU_CoS_Syllabi
+ * @package MU-CoS-Syllabi
  */
 
 /**
  * Enqueue scripts and styles.
  */
 function cos_syllabi_styles() {
-	wp_register_style( 'syllabi_datatables_style', plugins_url( 'mu-cos-syllabi/css/datatables.min.css' ), array(), false, 'all' ); // phpcs:ignore
-	wp_register_style( 'syllabi_style', plugins_url( 'mu-cos-syllabi/css/mu_cos_syllabi.css' ), array(), false, 'all' ); // phpcs:ignore
+	wp_enqueue_style( 'syllabi_datatables_style', plugin_dir_url( __FILE__ ) . 'css/mu-cos-syllabi.css', array(), filemtime( plugin_dir_path( __FILE__ ) . 'css/mu-cos-syllabi.css' ), 'all' );
+	wp_enqueue_style( 'syllabi_style', plugin_dir_url( __FILE__ ) . 'css/datatables.min.css', array(), filemtime( plugin_dir_path( __FILE__ ) . 'css/datatables.min.css' ), 'all' );
 	wp_enqueue_style( 'syllabi_datatables_style' );
 	wp_enqueue_style( 'syllabi_style' );
 }
@@ -24,7 +24,7 @@ function cos_syllabi_styles() {
  */
 function cos_syllabi_scripts() {
 	wp_enqueue_script( 'syllabi_datatables', 'https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js', array( 'jquery' ), false, true ); // phpcs:ignore
-	wp_enqueue_script( 'syllabi_js', plugins_url( 'mu_cos_syllabi/js/mu_cos_syllabi.js' ), array( 'jquery' ), false, true ); // phpcs:ignore
+	wp_enqueue_script( 'syllabi_js', plugin_dir_url( __FILE__ ) . 'js/mu-cos-syllabi.js', array( 'jquery' ), false, true ); // phpcs:ignore
 }
 
 add_action( 'wp_enqueue_scripts', 'cos_syllabi_styles' );
@@ -62,7 +62,7 @@ function cos_syllabi( $atts ) {
 	$course_list .= '</table></div>';
 
 	$course_script .= '<script type="text/javascript">';
-	$course_script .= 'var mu_cos_syllabiDept = ' . ( $dept_param ? '\'' . $dept_param . '\'' : 'null' ) . ';';
+	$course_script .= 'var mu-cos-syllabiDept = ' . ( $dept_param ? '\'' . $dept_param . '\'' : 'null' ) . ';';
 	$course_script .= '</script>';
 	$course_script .= '<noscript><p>This listing of courses and their syllabi can be very large. JavaScript is used to collect, display, sort, and search the listing. Please enable scripts and refresh the page.</p></noscript>';
 
@@ -71,4 +71,4 @@ function cos_syllabi( $atts ) {
 	return $result;
 }
 
-add_shortcode( 'mu_cos_syllabi', 'cos_syllabi' );
+add_shortcode( 'mu-cos-syllabi', 'cos_syllabi' );
